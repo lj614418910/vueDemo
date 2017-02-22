bc4bb2bc6142f09bd9c608c9ff39b137a84f44fd
lj614418910 <614418910@qq.com>
2017-02-22 10:47:52 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div>{{count}}</div>
+    <botton @click="increment">+</botton>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      count:0
+    }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
